25889bb0bb50727b1d6f6c84d805745a03409690
ibrahimsiddiqu <161191033+ibrahimsiddiqu@users.noreply.github.com>
2024-02-25 11:38:39 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//   function bisma( ){
+//    for(let i = 0; i<10; i++){
+//     console.log('my name is syed shaheryar');
+//    }
+// let a = 20;
+// let b = 60;
+// let c = a+b;
+//   console.log(c)
+//   }
+
+// bisma();
+
+// let add = function (){
+//     console.log('welcome')
+// }
+// add();
+
+// let add =()=>{
+// document.write('<h1>my name is abc</h1>')
+// }
+
